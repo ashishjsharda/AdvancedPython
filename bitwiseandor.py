73be783857730b024ace2e6a663cf31df939e2d7
Ashish S <ashishjsharda@gmail.com>
2019-10-12 15:30:08 -0400

    Create bitwiseandor.py

---
+++ bitwiseandor.py
@@ -0,0 +1,13 @@
+'''
+Created on Oct 12, 2019
+Bitwise and or
+@author: asharda
+'''
+import numpy as np
+
+a=13
+b=17
+print("Binary equivalent of 13 is",bin(a))
+print("Binary equivalent of 17 is",bin(b))
+print("Bitwise and is",np.bitwise_and(a,b))
+print("Bitwise OR is",np.bitwise_or(a,b))
