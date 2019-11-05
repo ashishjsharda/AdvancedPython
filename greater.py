b0b49f72725c48835402764851232516f6247b61
Ashish S <ashishjsharda@gmail.com>
2019-11-04 22:57:51 -0500

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,8 @@
+'''
+Created on Nov 4, 2019
+Find Numbers greater than in an array
+@author: asharda
+'''
+import numpy as np 
+x = np.array([[ 0,  1,  2],[ 3,  4,  5],[ 6,  7,  8],[ 9, 10, 11]])
+print(x[x>5]) 
