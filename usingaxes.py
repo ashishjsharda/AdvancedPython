99eb66e93aa70d6c19fc7b240cf4765074871ff2
Ashish S <ashishjsharda@gmail.com>
2020-01-14 23:17:58 -0500

    Create usingaxes.py

---
+++ usingaxes.py
@@ -0,0 +1,13 @@
+'''
+Created on Jan 14, 2020
+
+Using Axes
+@author: ashish
+'''
+import matplotlib.pyplot as plt
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.title("Using Axes")
+plt.show()
