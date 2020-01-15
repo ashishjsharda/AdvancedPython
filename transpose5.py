cc30027f0985e0eb0d81742bb7ee1c9caa3647da
Ashish S <ashishjsharda@gmail.com>
2020-01-14 23:03:36 -0500

    Create transpose5.py

---
+++ transpose5.py
@@ -0,0 +1,9 @@
+'''
+Created on Jan 14, 2020
+
+@author: ashish
+'''
+import numpy as np
+a=np.matrix([[1,2],[3,4]])
+print("Initial matrix seen is",a)
+print("Transposed matrix seen is",a.transpose())
