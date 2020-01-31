5d7bf57ea131c4d45c4fb6bcabfab46a30ca63e3
Ashish S <ashishjsharda@gmail.com>
2020-01-31 08:38:32 -0500

    Create matplotlib3.py

---
+++ matplotlib3.py
@@ -0,0 +1,11 @@
+'''
+Created on Jan 31, 2020
+Numpy Math Operations
+@author: ashish
+'''
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5])
+plt.ylabel("Y Label")
+plt.xlabel("X Label")
+plt.title("Using Matplotlib")
+plt.show()
